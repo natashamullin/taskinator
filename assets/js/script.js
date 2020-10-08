9a8c723e1b58386ade717cad35759acf23d1c41f
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-10-08 12:45:39 -0700

    made more stuff happen

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,15 @@ console.log(buttonE1);
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 ///task name
 var taskFormHandler = function(event) {
+    //check if input values are empty strings
+    if (!taskTypeInput || !taskaTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
 
+formEl.requestFullscreen(;)
 
-
-    var createTaskEl = function(taskDatabj) {
+    var createTaskEl = function(taskDatObj) {
         // creatlist item 
         var listItemEl = document.createElement("li");
         listItemEl.className = "task-item";
